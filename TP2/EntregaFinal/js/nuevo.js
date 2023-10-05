f13284ce47e6ec105c715bc95c0163fcac158f0b
Javier <javierondicol84@gmail.com>
2023-10-05 09:55:26 -0300

    botones posiciones puestos

--- TP2/EntregaFinal/js/nuevo.js
+++ TP2/EntregaFinal/js/nuevo.js
@@ -1,3 +1,4 @@
+
 "use strict"
 let btnizq = document.querySelector(".btn-left-cards");
 
@@ -7,24 +8,13 @@ let cincocards = document.querySelector(".cincocards");
 
 btnizq.classList.add('hidden');
 btnizq.addEventListener('click', function(){
-
-  
-  document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skew(-15deg)'; 
-  document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(0%)';
-  document.querySelector('.cincocards:nth-child(1)').style.transition = 'transform 1s ease'; 
-  document.querySelector('.cincocards:nth-child(2)').style.transition = 'transform 1s ease';
+    //cincocards.style.transform = `translate(-1300px)`;
+    document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skew(-15deg)'; 
+    document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(0%)';
 
   setTimeout(function() {
-     document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skewX(0deg)';
-   }, 500);
-
-  // document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-izquierda');
-  // document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(0%)';
-
-  // setTimeout(function() {
-  //   document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-izquierda');
-  //   document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-reset');
-  // }, 500);
+    document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skewX(0deg)';
+  }, 500);
 
   btnder.classList.remove('hidden');
   btnizq.classList.add('hidden');
@@ -35,8 +25,6 @@ btnder.addEventListener('click', function(){
   // cincocards.style.transform = `translate(1300px)`; 
   document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(-100%)';
   document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(-100%) skew(15deg)'; 
-  document.querySelector('.cincocards:nth-child(1)').style.transition = 'transform 1s ease'; 
-  document.querySelector('.cincocards:nth-child(2)').style.transition = 'transform 1s ease';
 
   setTimeout(function() {
     document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(-100%) skewX(0deg)';
@@ -45,3 +33,5 @@ btnder.addEventListener('click', function(){
   btnizq.classList.remove('hidden');
   btnder.classList.add('hidden');
 });
+
+
