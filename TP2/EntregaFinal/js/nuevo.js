d7a5a789f2a00534c7c3a8785b7ed947e9ea4482
Javier <javierondicol84@gmail.com>
2023-10-05 10:45:18 -0300

    clases puestas

--- TP2/EntregaFinal/js/nuevo.js
+++ TP2/EntregaFinal/js/nuevo.js
@@ -1,50 +1,50 @@
 
-"use strict"
+"use strict";
+
 let btnizq = document.querySelector(".btn-left-cards");
-
 let btnder = document.querySelector(".btn-right-cards");
-
-let cincocards = document.querySelector(".cincocards");
 
 btnizq.classList.add('hidden');
 btnizq.addEventListener('click', function(){
+ 
+    // Agrega las clases para la transformación al segundo elemento
+    document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-derecha');
+    document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
+    document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-reset-dos');
+    document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-izquierda');
+    document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-izquierda');
+  
+    setTimeout(function() {
+      // Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
+      document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
+      document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
+      document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-reset');
+  
+    }, 1000);
 
-    document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skew(-15deg)'; 
-    document.querySelector('.cincocards:nth-child(1)').style.transition = 'transform 1s ease'; 
-
-
-    document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(0%)';
-    document.querySelector('.cincocards:nth-child(2)').style.transition = 'transform 1s ease'; 
-
-//poner que solo despues de un segundo se puede dar click al otro boton, si no se chocan los cards
-
-  setTimeout(function() {
-    document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(0%) skewX(0deg)';
-  }, 1000);
 
   btnder.classList.remove('hidden');
   btnizq.classList.add('hidden');
-})
-
+});
 
 btnder.addEventListener('click', function(){
-
-  document.querySelector('.cincocards:nth-child(1)').style.transform = 'translateX(-100%)';
-  document.querySelector('.cincocards:nth-child(1)').style.transition = 'transform 1s ease'; 
-
-
-  document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(-100%) skew(5deg)'; 
-  document.querySelector('.cincocards:nth-child(2)').style.transition = 'transform 1s ease'; 
-
-
+  // Agrega las clases para la transformación al segundo elemento
+  document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-reset');
+  document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-izquierda');
+  document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
+  document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-derecha');
+  document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-derecha');
 
   setTimeout(function() {
-    document.querySelector('.cincocards:nth-child(2)').style.transform = 'translateX(-100%) skewX(0deg)';
-    document.querySelector('.cincocards:nth-child(2)').style.transition = 'transform 1s ease'; 
+    // Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
+    document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
+    document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
+    document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-reset');
+    document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-reset-dos');
+
+
   }, 1000);
 
   btnizq.classList.remove('hidden');
   btnder.classList.add('hidden');
 });
-
-
