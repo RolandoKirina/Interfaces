b088e31a843d4a7149692f6ad78733df3c45a4d0
Javier <javierondicol84@gmail.com>
2023-10-05 12:40:10 -0300

    poniendo imagenes

--- TP2/EntregaFinal/js/carruselesCards.js
+++ TP2/EntregaFinal/js/carruselesCards.js
@@ -1,56 +1,6 @@
 
 "use strict";
 
-// let btnizq = document.querySelector(".btn-left-cards");
-// let btnder = document.querySelector(".btn-right-cards");
-
-// btnizq.classList.add('hidden');
-// btnizq.addEventListener('click', function(){
- 
-//     // Agrega las clases para la transformación al segundo elemento
-//     document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-derecha');
-//     document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
-//     document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-reset-dos');
-//     document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-izquierda');
-//     document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-izquierda');
-  
-//     setTimeout(function() {
-//       // Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
-//       document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
-//       document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
-//       document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-reset');
-      
-//       btnder.classList.remove('hidden');
-//       btnizq.classList.add('hidden');
-//     }, 500);
-
-
- 
-// });
-
-// btnder.addEventListener('click', function(){
-//   // Agrega las clases para la transformación al segundo elemento
-//   document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-reset');
-//   document.querySelector('.cincocards:nth-child(1)').classList.remove('transformacion-izquierda');
-//   document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
-//   document.querySelector('.cincocards:nth-child(1)').classList.add('transformacion-derecha');
-//   document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-derecha');
-
-//   setTimeout(function() {
-//     // Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
-//     document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-izquierda');
-//     document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-derecha');
-//     document.querySelector('.cincocards:nth-child(2)').classList.remove('transformacion-reset');
-//     document.querySelector('.cincocards:nth-child(2)').classList.add('transformacion-reset-dos');
-
-//     btnizq.classList.remove('hidden');
-//   btnder.classList.add('hidden');
-//   }, 500);
-
-  
-// });
-
-
 let allBtnIzqs = document.querySelectorAll(".btn-left-cards");
 let allBtnDer = document.querySelectorAll(".btn-right-cards");
 
