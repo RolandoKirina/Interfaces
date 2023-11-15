6781035cd89423a624bf98fdd9bbd1cf135c2449
Kirina Rolando <kirinarolando@gmail.com>
2023-11-15 16:35:43 -0300

    agregando

--- TP4/EntregaFinal/js/main.js
+++ TP4/EntregaFinal/js/main.js
@@ -10,16 +10,33 @@ menu.addEventListener('click', function() {
 let title = document.querySelector("#title");
 
 let divtitle = document.querySelector("#divtitle");
+
 window.addEventListener("scroll", function (){
     let posY = window.scrollY;
-    let posicion = 100;
-    let postitle = 150;
-    let posfinal =160;
-    if (posY >= posicion) {
+    let title = document.getElementById("title");
+    let divtitle = document.getElementById("divtitle");
+    let scaleValue = 0.4;
+    let maxscale =0.9; 
+    let initialpos = 1;
+    let speed = 0.0017;
+    if (posY >= initialpos && posY <= 300) {
         title.classList.remove("maintitle");
         title.classList.add("reducetitle");
         divtitle.classList.remove("absolute");
         divtitle.classList.add("titleSticky");
-    } 
+        scaleValue = Math.max(scaleValue, maxscale - ((posY - initialpos) * speed),0.4);
+         /* se resta la escala maxima menos la posy actual menos la pos y
+          inicial y se multiplica por la velocidad. se queda con el numero mas grande*/
+        title.style.transform = `scale(${scaleValue})`;
+
+    } else if (posY > 300 ) {
+        title.classList.remove("maintitle");
+        title.classList.remove("reducetitle");
+        divtitle.classList.remove("absolute");
+        scaleValue = Math.max(scaleValue, 0.4);
+        title.style.transform = `scale(${scaleValue})`;
+    }
     console.log(posY);
 });
+
+
